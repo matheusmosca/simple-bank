b7e754bf472ea869a0ac04134e17cf0dac14d77a
Matheus Mosca <matheusmoscag@gmail.com>
2021-05-01 20:35:02 -0300

    refactor: change NewAccount return statement

--- pkg/domain/entities/account.go
+++ pkg/domain/entities/account.go
@@ -19,14 +19,26 @@ type Account struct {
 	CreatedAt time.Time
 }
 
-func NewAccount(name, CPF, secret string, balance int) *Account {
-	// TODO validate cpf
-	// TODO hash secret with bcrypt
-	return &Account{
+func NewAccount(name, CPF, secret string, balance int) (*Account, error) {
+	acc := Account{
 		ID:      newID(),
 		Name:    name,
 		CPF:     CPF,
 		Secret:  secret,
 		Balance: balance,
 	}
+	err := acc.Validate()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &acc, nil
+}
+
+// TODO add proper validation
+// TODO validate cpf
+// TODO hash secret with bcrypt
+func (a Account) Validate() error {
+	return nil
 }
