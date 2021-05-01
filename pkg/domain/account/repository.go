64f5d5023ba3822f19c2f2b85da37e28059e8ef4
Matheus Mosca <matheusmoscag@gmail.com>
2021-05-01 20:31:38 -0300

    feat: add account repository interface

---
+++ pkg/domain/account/repository.go
@@ -0,0 +1,14 @@
+package account
+
+import (
+	"context"
+	"simple-bank/pkg/domain/entities"
+)
+
+type Repository interface {
+	Create(ctx context.Context, account *entities.Account) error
+	GetByID(ctx context.Context, accountID string) (*entities.Account, error)
+	GetByCPF(ctx context.Context, CPF string) (*entities.Account, error)
+	GetAccounts(ctx context.Context) ([]*entities.Account, error)
+	UpdateBalance(ctx context.Context, ID string) error
+}
