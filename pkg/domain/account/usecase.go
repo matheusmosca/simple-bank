ddf03663831cb9ac16d14a736f0ccf463b13add2
Matheus Mosca <matheusmoscag@gmail.com>
2021-05-01 20:30:39 -0300

    feat: add account usecase interface

---
+++ pkg/domain/account/usecase.go
@@ -0,0 +1,16 @@
+package account
+
+import (
+	"context"
+	"errors"
+	"simple-bank/pkg/domain/entities"
+)
+
+var (
+	ErrCPFAlreadyExists = errors.New(`the cpf is already in use`)
+)
+
+type UseCase interface {
+	// List(ctx context.Context) ([]*entities.Account, error)
+	Create(ctx context.Context, name, CPF, secret string, balance int) (*entities.Account, error)
+}
