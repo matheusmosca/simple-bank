59b6d059667deac4208e18229ccabb956e6015dc
Matheus Mosca <matheusmoscag@gmail.com>
2021-05-01 21:06:55 -0300

    feat: add db connection in main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,51 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "simple_bank_db"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "simple_bank_db"
 )
 
 func main() {
-	fmt.Println(".")
+	psqlConfig := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlConfig)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+
+	if err != nil {
+		panic(err)
+	}
+
+	// accountUseCase := account_usecase.NewAccountUseCase(account_postgree.NewRepository(db))
+
+	// acc, err := accountUseCase.Create(context.Background(), "maria", "738.333.222-12", "2222222222", 0)
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+
+	// log.Println(acc)
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/api/v1", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello")
+	})
+
+	http.ListenAndServe(":3000", r)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module simple-bank
 
 go 1.16
 
-require github.com/google/uuid v1.2.0 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.2.0
+	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.1
+	github.com/stretchr/testify v1.7.0
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
