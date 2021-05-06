cb909ee9d242b4f5cd22aa4724c3982bd7353535
Matheus Mosca <matheusmoscag@gmail.com>
2021-05-06 15:26:57 -0300

    tests: add tests to createAccount handler

---
+++ pkg/gateways/http/account/create_account_test.go
@@ -0,0 +1,137 @@
+package account
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"simple-bank/pkg/domain/entities"
+	"simple-bank/pkg/gateways/http/util/response"
+	"testing"
+)
+
+type noBody struct {
+	Message string `json:"message"`
+}
+
+func TestCreateAccount(t *testing.T) {
+	path := "/api/v1/accounts"
+
+	t.Run("Should return 201 and create an account", func(t *testing.T) {
+		acc, _ := entities.NewAccount("jorge", "685.490.160-04", "12345678")
+
+		handler := fakeHandler(mockResponse{
+			CreateAccountErr:     nil,
+			CreateAccountPayload: acc,
+		})
+
+		request := fakeRequest(http.MethodPost, path, CreateBodyRequest{
+			Name:   acc.Name,
+			CPF:    acc.CPF,
+			Secret: "12345678",
+		})
+
+		wantResponseBody, _ := json.Marshal(ResponseBody{
+			ID:        acc.ID,
+			CPF:       acc.CPF,
+			Name:      acc.Name,
+			Balance:   acc.DisplayBalance(),
+			CreatedAt: acc.CreatedAt,
+		})
+
+		response := httptest.NewRecorder()
+
+		http.HandlerFunc(handler.Create).ServeHTTP(response, request)
+
+		assertResponseHelper(t, wantResponseBody, http.StatusCreated, response)
+	})
+
+	t.Run("Should return 400 due to a empty body", func(t *testing.T) {
+		requestBody := ""
+
+		wantResponseBody, _ := json.Marshal(noBody{Message: "invalid params"})
+
+		handler := fakeHandler(mockResponse{})
+
+		request := fakeRequest(http.MethodPost, path, requestBody)
+
+		response := httptest.NewRecorder()
+
+		http.HandlerFunc(handler.Create).ServeHTTP(response, request)
+
+		assertResponseHelper(t, wantResponseBody, http.StatusBadRequest, response)
+	})
+
+	t.Run("Should return 400 due to invalid fields", func(t *testing.T) {
+		requestBody := CreateBodyRequest{
+			Secret: "12345677",
+		}
+
+		wantResponseBody, _ := json.Marshal(ValidationErrorResponse{
+			CPF:  "cpf is a required field",
+			Name: "name is a required field",
+		})
+
+		handler := fakeHandler(mockResponse{})
+
+		request := fakeRequest(http.MethodPost, path, requestBody)
+
+		response := httptest.NewRecorder()
+
+		http.HandlerFunc(handler.Create).ServeHTTP(response, request)
+
+		assertResponseHelper(t, wantResponseBody, http.StatusBadRequest, response)
+	})
+
+	t.Run("Should return 400 due to usecase validation error", func(t *testing.T) {
+		requestBody := CreateBodyRequest{
+			//? Invalid CPF
+			CPF:    "123.456.123-12",
+			Name:   "jorge",
+			Secret: "12345677",
+		}
+
+		wantResponseBody, _ := json.Marshal(response.ErrorResponse{
+			Message: entities.ErrInvalidCPF.Error(),
+		})
+
+		handler := fakeHandler(mockResponse{
+			CreateAccountErr:     entities.ErrInvalidCPF,
+			CreateAccountPayload: nil,
+		})
+
+		request := fakeRequest(http.MethodPost, path, requestBody)
+
+		response := httptest.NewRecorder()
+
+		http.HandlerFunc(handler.Create).ServeHTTP(response, request)
+
+		assertResponseHelper(t, wantResponseBody, http.StatusBadRequest, response)
+	})
+
+	t.Run("Should return 500 due to an unexpected behavior", func(t *testing.T) {
+		// Valid request body
+		requestBody := CreateBodyRequest{
+			CPF:    "685.490.160-04",
+			Name:   "jorge",
+			Secret: "12345677",
+		}
+
+		wantResponseBody, _ := json.Marshal(response.ErrorResponse{
+			Message: response.ErrIntervalServer,
+		})
+
+		handler := fakeHandler(mockResponse{
+			CreateAccountErr:     errors.New("something went wrong"),
+			CreateAccountPayload: nil,
+		})
+
+		request := fakeRequest(http.MethodPost, path, requestBody)
+
+		response := httptest.NewRecorder()
+
+		http.HandlerFunc(handler.Create).ServeHTTP(response, request)
+
+		assertResponseHelper(t, wantResponseBody, http.StatusInternalServerError, response)
+	})
+}
---
+++ pkg/gateways/http/account/setup_test.go
@@ -0,0 +1,62 @@
+package account
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"simple-bank/pkg/domain/account"
+	"simple-bank/pkg/domain/entities"
+	"simple-bank/pkg/gateways/http/util/validator"
+)
+
+type mockResponse struct {
+	CreateAccountErr      error
+	CreateAccountPayload  *entities.Account
+	GetByIDAccountErr     error
+	GetByIDAccountPayload *entities.Account
+	ListErr               error
+	ListPayload           []entities.Account
+}
+
+func fakeHandler(res mockResponse) Handler {
+	mockAccountUseCase := &account.UseCaseMock{
+		CreateFunc: func(ctx context.Context, input entities.CreateAccountInput) (*entities.Account, error) {
+			return res.CreateAccountPayload, res.CreateAccountErr
+		},
+		GetByIDFunc: func(ctx context.Context, accountID string) (*entities.Account, error) {
+			return res.GetByIDAccountPayload, res.GetByIDAccountErr
+		},
+		ListFunc: func(ctx context.Context) ([]entities.Account, error) {
+			return res.ListPayload, res.ListErr
+		},
+	}
+
+	return Handler{
+		usecase:   mockAccountUseCase,
+		validator: validator.New(),
+	}
+}
+
+func fakeRequest(method, path string, body interface{}) *http.Request {
+	reqBody, err := json.Marshal(body)
+	if err != nil {
+		log.Println(err)
+	}
+	req := bytes.NewReader(reqBody)
+	return httptest.NewRequest(method, path, req)
+}
+
+// test helper to assert a response
+func assertResponseHelper(t *testing.T, wantBody []byte, wantHTTPMethod int, res *httptest.ResponseRecorder) {
+	assert.Equal(t, string(wantBody), strings.TrimSpace(res.Body.String()))
+	assert.Equal(t, wantHTTPMethod, res.Code)
+	assert.Equal(t, "application/json", res.Header().Get("Content-Type"))
+}
