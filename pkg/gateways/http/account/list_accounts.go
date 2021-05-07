2ab0cb4a35fefa8ea70ffa82bb2b874e36705e4c
Matheus Mosca <matheusmoscag@gmail.com>
2021-05-06 21:59:32 -0300

    fix: fix listAccount handler response payload

--- pkg/gateways/http/account/list_accounts.go
+++ pkg/gateways/http/account/list_accounts.go
@@ -2,17 +2,12 @@ package account
 
 import (
 	"net/http"
-	"simple-bank/pkg/domain/account"
 	"simple-bank/pkg/gateways/http/util/response"
 )
 
 func (h Handler) List(w http.ResponseWriter, r *http.Request) {
 	accounts, err := h.usecase.List(r.Context())
 	if err != nil {
-		if account.IsDomainError(err) {
-			response.SendError(w, err.Error(), http.StatusNotFound)
-			return
-		}
 		response.SendError(w, response.ErrIntervalServer, http.StatusInternalServerError)
 		return
 	}
@@ -22,6 +17,7 @@ func (h Handler) List(w http.ResponseWriter, r *http.Request) {
 	for i, acc := range accounts {
 		res[i] = ResponseBody{
 			ID:        acc.ID,
+			Name:      acc.Name,
 			CPF:       acc.CPF,
 			Balance:   acc.DisplayBalance(),
 			CreatedAt: acc.CreatedAt,
