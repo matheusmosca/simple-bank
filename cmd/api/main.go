b1be034941beeb46e25c4fbd300d8d98b98ebc01
Matheus Mosca <matheusmoscag@gmail.com>
2021-05-06 21:14:41 -0300

    fix: fix main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 
+	"simple-bank/pkg/common/configuration"
 	account_usecase "simple-bank/pkg/domain/account/usecase"
 	auth_service "simple-bank/pkg/domain/auth/service"
 	"simple-bank/pkg/gateways/db/postgres"
@@ -15,40 +14,27 @@ import (
 	"simple-bank/pkg/gateways/http"
 )
 
-const (
-	host     = "simple_bank_db"
-	port     = 5432
-	user     = "postgres"
-	password = "postgres"
-	dbname   = "simple_bank_db"
-)
-
 func main() {
-	err := godotenv.Load()
+	// Load Config
+	cfg, err := configuration.LoadConfig()
 	if err != nil {
-		log.Println("Error loading .env file")
+		log.Fatal("Unable to load credit-fs-api configuration")
 	}
 
-	psqlConfig := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-
-	connectionString := fmt.Sprintf("postgres://%s:%s@%s:%v/%s?sslmode=%s",
-		user, password, host, port, dbname, "disable")
-
-	db, err := sql.Open("postgres", psqlConfig)
+	db, err := sql.Open("postgres", cfg.DSN())
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer db.Close()
 	err = db.Ping()
 
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	err = postgres.RunMigrations(connectionString)
+	err = postgres.RunMigrations(cfg.URL())
 	if err != nil {
-		log.Println(err)
+		log.Fatal(err)
 	}
 
 	accountUseCase := account_usecase.NewAccountUseCase(account_postgre.NewRepository(db))
@@ -56,5 +42,5 @@ func main() {
 
 	API := http.NewAPI(accountUseCase, authService)
 
-	API.Start()
+	API.Start("0.0.0.0",cfg.API.Port)
 }
