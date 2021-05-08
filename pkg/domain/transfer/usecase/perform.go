acc69322b1f761da832065ae1f455efffc6e0e8b
Matheus Mosca <matheusmoscag@gmail.com>
2021-05-07 22:51:03 -0300

    feat: add PerformTransference usecase

--- pkg/domain/transfer/usecase/perform.go
+++ pkg/domain/transfer/usecase/perform.go
@@ -6,5 +6,43 @@ import (
 )
 
 func (t Transfer) Perform(ctx context.Context, input entities.CreateTransferInput) (*entities.Transfer, error) {
-	return nil, nil
+	transfer, err := entities.NewTransfer(
+		input.AccountOriginID,
+		input.AccountDestinationID,
+		input.Amount,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	origAcc, err := t.accountUseCase.GetByID(ctx, input.AccountOriginID)
+	if err != nil {
+		return nil, entities.ErrOrigAccountDoesNotExist
+	}
+
+	destAcc, err := t.accountUseCase.GetByID(ctx, input.AccountDestinationID)
+	if err != nil {
+		return nil, entities.ErrDestAccountDoesNotExist
+	}
+
+	err = origAcc.WithdrawMoney(input.Amount)
+	if err != nil {
+		return nil, err
+	}
+
+	err = destAcc.DepositMoney(input.Amount)
+	if err != nil {
+		return nil, err
+	}
+
+	err = t.repository.PerformTransference(ctx, entities.PerformTransferenceInput{
+		OriginAcount:      origAcc,
+		DestinationAcount: destAcc,
+		Transfer:          transfer,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return transfer, nil
 }
